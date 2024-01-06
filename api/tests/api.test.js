b370c7ae072140387a9fe2c42dcc8e10181f89ca
SoVa-web <oolga200000001@ukr.net>
2024-01-06 12:02:51 +0200

    add test config

---
+++ api/tests/api.test.js
@@ -0,0 +1,8 @@
+const { /* name function for testing */ } = require(/* directory */); // example '../src/calculator'
+
+// example
+describe('Calculator', () => {
+  it('should add two numbers correctly', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
